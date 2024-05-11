c50961710375fb67e4df1b73aa07fed248327e9c
Winwin Kobayashi <victoriakobayashi553@gmail.com>
2024-05-11 11:46:01 +0800

    added login, logout, user roles

---
+++ php/connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "doughpro";
+
+try {
+    $connection = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $th) {
+    echo "Connection failed: " . $th->getMessage();
+}
+?>
