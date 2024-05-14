892b34657543473dc0a3c831f113fd7edd976a51
Winwin Kobayashi <victoriakobayashi553@gmail.com>
2024-05-14 23:58:12 +0800

    added crud users page; created sidebar files; added button styles css;

--- php/admin-crud-users-page.php
+++ php/admin-crud-users-page.php
@@ -19,6 +19,7 @@ $user_id = $_SESSION["user_id"];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DoughPro</title>
     <link rel="stylesheet" href="../css/dashboard.css" />
+    <link rel="stylesheet" href="../css/button-styles.css" />
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="icon" type="image/x-icon" href="../default-images/baguette-solid-24.png">
 </head>
@@ -27,7 +28,13 @@ $user_id = $_SESSION["user_id"];
     <?php include('admin-sidebar.php'); ?>
 
     <section class="home">
-        <!-- <div class="text">Dashboard Sidebar</div> -->
+        <div class="text">Users</div>
+
+        <button class="btn-create">
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path></svg> Add User
+            </span>
+        </button>
     </section>
 
     <script src="../js/admin-dashboard.js"></script>
--- php/staff-dashboard.php
+++ php/staff-dashboard.php
@@ -23,79 +23,8 @@
     <link rel="icon" type="image/x-icon" href="../default-images/baguette-solid-24.png">
 </head>
 <body>
-    <nav class="sidebar close">
-        <header>
-            <div class="image-text">
-                <span class="image">
-                    <i class='bx bx-baguette doughpro-icon' ></i>
-                </span>
-
-                <div class="text logo-text">
-                    <span class="name">DoughPro</span>
-                    <span class="profession">Staff - <span class="user-name">  <?php echo htmlspecialchars($user_name); ?> </span> </span>
-                </div>
-            </div>
-
-            <i class='bx bx-chevron-right toggle'></i>
-        </header>
-
-        <div class="menu-bar">
-            <div class="menu">
-
-                <li class="search-box">
-                    <i class='bx bx-search icon'></i>
-                    <input type="text" placeholder="Search...">
-                </li>
-
-                <ul class="menu-links">
-                    <li class="nav-link">
-                        <a href="#">
-                            <i class='bx bx-home-alt icon' ></i>
-                            <span class="text nav-text">Stocks</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-link">
-                        <a href="#">
-                            <i class='bx bxs-shopping-bag icon'></i>
-                            <span class="text nav-text">To Order</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-link">
-                        <a href="#">
-                            <i class='bx bx-pie-chart-alt icon' ></i>
-                            <span class="text nav-text">Logs</span>
-                        </a>
-                    </li>
-
-                </ul>
-            </div>
-
-            <div class="bottom-content">
-                <li class="">
-                    <a href="logout.php">
-                        <i class='bx bx-log-out icon' ></i>
-                        <span class="text nav-text">Logout</span>
-                    </a>
-                </li>
-
-                <li class="mode">
-                    <div class="sun-moon">
-                        <i class='bx bx-moon icon moon'></i>
-                        <i class='bx bx-sun icon sun'></i>
-                    </div>
-                    <span class="mode-text text">Dark mode</span>
-
-                    <div class="toggle-switch">
-                        <span class="switch"></span>
-                    </div>
-                </li>
-                
-            </div>
-        </div>
-
-    </nav>
+   
+    <?php include("staff-sidebar.php") ?>
 
     <section class="home">
         <!-- <div class="text">Dashboard Sidebar</div> -->
