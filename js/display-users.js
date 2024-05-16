9377dd949b5e9c2d7c5e33181a4da16f0de93f7a
Winwin Kobayashi <victoriakobayashi553@gmail.com>
2024-05-16 12:51:11 +0800

    + populate UpdateUserModal with selected data

--- js/display-users.js
+++ js/display-users.js
@@ -41,3 +41,21 @@ $(document).ready(function() {
     loadData();
 });
 
+
+// // POPULATE MODAL FORM update users
+// $(document).on("click", ".btn-update", function() {
+//     var user_id = $(this).attr("id");
+//     var user_name = $(this).data("user_name");
+//     var user_role = $(this).data("user_role");
+//     var user_email = $(this).data("user_email");
+//     // var currentPictureUrl = $(this).closest(".card").find(".card-img-top").attr("src");
+
+//     // // Set values in the update modal
+//     $("#update_user_id").val(user_id);
+//     $("#udpate_username").val(user_name);
+//     $("#update_role").val(user_role);
+//     $("#update_email").val(user_email);
+
+//     $("#updateUserModal").modal("show");
+// });
+
--- js/update-users.js
+++ js/update-users.js
@@ -1,16 +1,55 @@
 // UPDATE USERS
+
+// populate the Update Users Modal with data
 $(document).on("click", ".btn-update", function() {
     var user_id = $(this).attr("id");
-    var user_name = $(this).data("user_name");
-    var user_role = $(this).data("user_role");
-    var user_email = $(this).data("user_email");
-    // var currentPictureUrl = $(this).closest(".card").find(".card-img-top").attr("src");
+    var user_name = $(this).data("user-name");
+    var user_role = $(this).data("user-role");
+    var user_email = $(this).data("user-email");
 
     // // Set values in the update modal
     $("#update_user_id").val(user_id);
-    $("#udpate_username").val(user_name);
+    $("#update_username").val(user_name);
     $("#update_role").val(user_role);
     $("#update_email").val(user_email);
 
     $("#updateUserModal").modal("show");
+});
+
+// update the database
+$("#btnUpdateProduct").click(function() {
+    var productId = $("#updateProductId").val();
+    var productName = $("#updateProductName").val();
+    var productCategory = $("#updateProductCategory").val();
+    var productQuantity = $("#updateProductQuantity").val();
+
+    var formData = new FormData();
+    var picture = $("#updatePicture")[0].files[0];
+
+    // Append form data
+    formData.append('id', productId);
+    formData.append('name', productName);
+    formData.append('category', productCategory);
+    formData.append('quantity', productQuantity);
+
+    // Append picture
+    formData.append('picture', picture);
+
+    if (productName.length > 0 && productCategory.length > 0 && productQuantity.length > 0) {
+        $.ajax({
+            url: "products.update.php",
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+        }).done(function(data) {
+            let result = JSON.parse(data);
+            if (result.res == "success") {
+                location.reload();
+            }
+        }).fail(function(xhr, status, error) {
+            console.error("An error occurred while updating product:", error);
+            alert("An error occurred while updating product. Please try again later.");
+        });
+    }
 });
