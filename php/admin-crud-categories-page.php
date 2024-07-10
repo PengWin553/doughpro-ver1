73a7690183487145f2b330b9bebfce08c77dc38e
PengWin553 <158315168+PengWin553@users.noreply.github.com>
2024-07-10 19:12:20 +0800

    Updated Categories Page
    Fixed Pagination Design

--- php/admin-crud-categories-page.php
+++ php/admin-crud-categories-page.php
@@ -25,38 +25,10 @@ $user_id = $_SESSION["user_id"];
     <link rel="stylesheet" href="../css/table-styles.css" />
     <link rel="stylesheet" href="../css/page-title-styles.css" />
     <link rel="stylesheet" href="../css/page-title-styles.css" />
+    <link rel="stylesheet" href="../css/pagination-styles.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="icon" type="image/x-icon" href="../default-images/baguette-solid-24.png">
-    <style>
-        /* Add this to your CSS file */
-.pagination-controls {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 20px;
-}
-
-.pagination-controls button {
-    margin: 0 10px;
-    padding: 5px 10px;
-    background-color: #007bff;
-    color: white;
-    border: none;
-    border-radius: 3px;
-    cursor: pointer;
-}
-
-.pagination-controls button:disabled {
-    background-color: #ccc;
-    cursor: not-allowed;
-}
-
-.pagination-controls span {
-    font-size: 16px;
-}
-
-    </style>
 </head>
 
 <body>
@@ -157,7 +129,7 @@ $user_id = $_SESSION["user_id"];
         </div>
 
         <!-- Pagination Controls -->
-        <div id="pagination-controls" class="pagination-controls" style="background: yellow; width: 100%; display: flex;">
+        <div id="pagination-controls" class="pagination-controls" >
             <!-- Pagination buttons will be dynamically inserted here by JavaScript -->
         </div>
     </section>
